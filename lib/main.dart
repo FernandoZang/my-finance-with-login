88411bd8d80913506069b5c63972bb41846f217e
fzang <fzang@uolinc.com>
2025-05-17 16:22:55 -0300

    Config firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,22 @@ import 'package:my_finances/screens/splash.dart';
 import 'package:my_finances/screens/entrada.dart';
 import 'package:my_finances/screens/saidas.dart';
 import 'package:my_finances/screens/onboarding.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  
   const MyApp({super.key});
 
   @override
